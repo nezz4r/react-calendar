928c2edd50398ccfc4934c08b85532c425bb80f1
Raul Queiroz <raulvictor00@outlook.com>
2021-03-25 19:44:46 -0300

    feat: changes icons

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -2,8 +2,12 @@ import styled from 'styled-components';
 import {
   BsChevronCompactLeft,
   BsChevronCompactRight,
-  BsThreeDotsVertical,
+  BsPlus,
 } from 'react-icons/bs';
+
+import { GrClose } from 'react-icons/gr';
+
+import { MdDelete } from 'react-icons/md';
 
 export const ArrowRightIcon = styled(BsChevronCompactRight)`
   color: inherit;
@@ -15,13 +19,27 @@ export const ArrowRightIcon = styled(BsChevronCompactRight)`
 export const ArrowLeftIcon = styled(BsChevronCompactLeft)`
   color: inherit;
   font-size: inherit;
-  transform: translateY(5%) rotate;
+  transform: translateY(5%);
   cursor: pointer;
 `;
 
-export const ThreeDots = styled(BsThreeDotsVertical)`
+export const PlusIcon = styled(BsPlus)`
   color: inherit;
   font-size: inherit;
-  transform: translateY(5%) rotate;
+  transform: translateY(5%);
+  cursor: pointer;
+`;
+
+export const CloseIcon = styled(GrClose)`
+  color: inherit;
+  font-size: inherit;
+  transform: translateY(5%);
+  cursor: pointer;
+`;
+
+export const DeleteIcon = styled(MdDelete)`
+  color: inherit;
+  font-size: inherit;
+  transform: translateY(5%);
   cursor: pointer;
 `;
